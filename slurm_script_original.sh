a35b9b80bf2c17790926028c2e28e7830a44a038
Hoang Nguyen <97536835+hoang-nguyen13@users.noreply.github.com>
2025-03-27 20:58:05 +0100

    Update slurm_script_original.sh

--- slurm_script_original.sh
+++ slurm_script_original.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 #SBATCH -J Rydberg
-#SBATCH --cpus-per-task=16
+#SBATCH --cpus-per-task=1
 #SBATCH -t 50:00:00
-#SBATCH -p epyc-256
+#SBATCH -p physik-fleischhauer
 #SBATCH -e err/%x_%A_%a.err
 #SBATCH -o out/%x_%A_%a.out
-#SBATCH --mem=0
+#SBATCH --mem-per-cpu 8G
 #SBATCH --array=0-20
 
 id=${SLURM_ARRAY_TASK_ID}
 
 # Run Julia with omega range
-~/julia-1.11.2/bin/julia -t 16 main.jl ${id}
+~/julia-1.11.2/bin/julia main.jl ${id}
