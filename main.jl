389dd6e0f539915d499d40e19d9f9491815aa134
hoang-nguyen13 <trollsherlock@gmail.com>
2025-04-30 21:17:31 +0200

    data

--- main.jl
+++ main.jl
@@ -111,9 +111,9 @@ function computeTWA(nAtoms, tf, nT, nTraj, Ω, Δ, V, Γ, γ, case)
                 saveat=tSave, 
                 trajectories=nTraj, 
                 maxiters=1e7, 
-                abstol=1e-3,
-                reltol=1e-3, 
-                dtmax=0.001)
+                abstol=1e-4,
+                reltol=1e-4, 
+                dtmax=0.005)
     
     sol_array = zeros(2 * nAtoms, nT, nTraj)
     for i in 1:nTraj
