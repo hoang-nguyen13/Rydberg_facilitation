9c92073280c621f1d58aa6e6ccbdb5e733ee61a9
hoang-nguyen13 <trollsherlock@gmail.com>
2025-05-21 20:32:53 +0200

    data

--- main.jl
+++ main.jl
@@ -107,12 +107,14 @@ function computeTWA(nAtoms, tf, nT, nTraj, Ω, Δ, V, Γ, γ, case)
         remake(prob, u0=u0)
     end)
     
-    sol = solve(ensemble_prob, EM(), EnsembleThreads(); 
+    sol = solve(ensemble_prob, SRIW1(), EnsembleThreads(); 
                 saveat=tSave, 
                 trajectories=nTraj,
 		abstol=1e-3,
 		reltol=1e-3,
-		dt=1e-3
+		dtmin=0.0001,
+		dtmax=0.00000000001,
+		maxiters=1e7
 		)
 
     sol_array = zeros(2 * nAtoms, nT, nTraj)
