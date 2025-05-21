57d8f7fb081296d1da9263fbc99177d2ad84e81c
hoang-nguyen13 <trollsherlock@gmail.com>
2025-05-21 19:37:06 +0200

    data

--- main.jl
+++ main.jl
@@ -107,7 +107,7 @@ function computeTWA(nAtoms, tf, nT, nTraj, Ω, Δ, V, Γ, γ, case)
         remake(prob, u0=u0)
     end)
     
-    sol = solve(ensemble_prob, SROCK1(), EnsembleThreads(); 
+    sol = solve(ensemble_prob, SKSROCK(), EnsembleThreads(); 
                 saveat=tSave, 
                 trajectories=nTraj,
 		abstol=1e-3,
