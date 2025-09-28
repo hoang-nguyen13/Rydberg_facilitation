a27bc1b82a00eca571887ac041d2bdba4216beb9
hoang-nguyen13 <trollsherlock@gmail.com>
2025-09-28 17:30:19 +0200

    data

--- submit_job.sh
+++ submit_job.sh
@@ -3,9 +3,9 @@
 declare -A OMEGA_RANGES
 
 #OMEGA_RANGES[20]="$(seq 0 2 29 | xargs -n1 printf '%.2f\n')"
-OMEGA_RANGES[20]="$(seq 10 0.15 13.15 | xargs -n1 printf '%.2f\n')"
+OMEGA_RANGES[5]="$(seq 10 0.15 13 | xargs -n1 printf '%.2f\n')"
 
-GAMMA_DEPHASING_VALUES=(20)
+GAMMA_DEPHASING_VALUES=(5)
 
 PAIRS=()
 for gamma_dephasing in "${GAMMA_DEPHASING_VALUES[@]}"; do
